a2a43bbd1fcbdc32a96765df1f2628424877551c
Precious Aigbiremhon <precious.impacts@yahoo.com>
2022-08-26 00:12:58 +0100

    Rework Assignment

---
+++ main.js
@@ -0,0 +1,24 @@
+let opr=parseInt(prompt("welcome to *894# banking\n 1>Quick Banking\n 2>Open an account\n 3>Loans\n 4>FirstMpnie\n 5>Payattitude "))
+
+
+if(opr===1){
+    
+    let QuickBank=parseInt(prompt("welcome to *894# banking\n 1> Transfer money\n 2> Airtime (self)\n 3> Airtime (others)\n 4> Enquiry services\n 5> Data\n 6> Pay bills\n 7> Next page"))
+
+    if(QuickBank===1){
+
+        alert("service not available")
+
+    }else if(QuickBank===2){
+
+        let Airtime=parseInt(prompt("Enter amount"))
+
+        alert("Successful. you can now increase your\n transfer limit from the comfort of your\n home")
+    }
+
+    
+
+    
+
+}
+
