1c76e4cb28670aa1eed6ea51e56d2d6045c9951a
Precious Aigbiremhon <precious.impacts@yahoo.com>
2022-09-04 00:47:29 +0100

    Ussd UI

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
-let opr=parseInt(prompt("welcome to *894# banking\n 1>Quick Banking\n 2>Open an account\n 3>Loans\n 4>FirstMpnie\n 5>Payattitude "))
+// let 
+let opr
+let option
+let transfer
+welcomePrompt()
+transaction1()
+transaction2()
+function welcomePrompt() {
 
+    opr = parseInt(prompt("welcome to *894# banking\n 1>Quick Banking\n 2>Open an account\n 3>Loans\n 4>FirstMpnie\n 5>Payattitude "))
 
-if(opr===1){
-    
-    let QuickBank=parseInt(prompt("welcome to *894# banking\n 1> Transfer money\n 2> Airtime (self)\n 3> Airtime (others)\n 4> Enquiry services\n 5> Data\n 6> Pay bills\n 7> Next page"))
+        transaction1()
+}   
+function transaction1(){
+    if (opr == 1) {
+        option = parseInt(prompt("welcome to *894# banking\n 1> Transfer money\n 2> Airtime (self)\n 3> Airtime (others)\n 4> Enquiry services\n 5> Data\n 6> Pay bills\n 7> Next page"))
+    } 
+    transaction2
+}
+function transaction2(){
+    let airtimeS
+    if (option===1){
 
-    if(QuickBank===1){
+    alert("service not available")
 
-        alert("service not available")
-
-    }else if(QuickBank===2){
-
-        let Airtime=parseInt(prompt("Enter amount"))
-
+    }  else if(option=== 2 ){
+        airtimeS = parseInt(prompt("Enter amount"))
         alert("Successful. you can now increase your\n transfer limit from the comfort of your\n home")
     }
-
-    
-
-    
-
-}
-
+}  
---
+++ ussd.js
@@ -0,0 +1,59 @@
+let fNum=document.getElementById("fNum")
+let btnCan=document.getElementById("btnCan")
+let btnSd=document.getElementById("btnSd")
+let fnumError=document.getElementById("fnumError")
+let qbankError=document.getElementById("qbankError")
+let airtimeO=document.getElementById("airTimeO")
+let enquiry=document.getElementById("enquiry")
+let data=document.getElementById("data")
+let pBills=document.getElementById("pBills")
+let nPage=document.getElementById("nPage")
+let transfer=document.getElementById("transfer")
+let airtimeS=document.getElementById("airtimeS")
+let qBank=document.getElementById("qBank")
+
+let amt=document.getElementById("amt")
+let amtError=document.getElementById("amtError")
+let pNum=document.getElementById("pNum")
+let pnumError=document.getElementById("pnumError")
+
+btnSd.addEventListener("click", function(){
+    validateform()
+})
+
+function validateform(){
+    if(qBank.value==""){
+        qbankError.innerHTML="Please, select option";
+    } else{
+        qbankError.innerHTML=""
+    }
+    
+    if(amt.value==""){
+        amtError.innerHTML="Please, Enter Amount";
+    }else {
+        amtError.innerHTML=""
+    }
+
+    if(pNum.value==""){
+        pnumError.innerHTML="Please, Enter Amount";
+    }else {
+        pnumError.innerHTML=""
+    }
+    if(qBank.value!="" && amt.value!="" && pNum.value!=""){
+        acceptValues()
+    }
+    // if(fNum.value==""){
+    //     fNum.value="";
+    //     return false
+}
+
+// function acceptValues(){
+//     fNum=parseInt(fNum.value)
+
+//     console.log(num1);
+//     console.log(num2);
+//     console.log(opr);
+
+//      calculate()
+//     }
+// }
